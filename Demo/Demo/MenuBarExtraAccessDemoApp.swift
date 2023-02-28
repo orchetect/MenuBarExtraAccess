6596dcc4075d3745cbc0e617ceb5a31833000e68
Steffan Andrews <stef@orchetect.com>
2023-02-27 16:10:13 -0800

    Updated demo with explainer text

--- Demo/Demo/MenuBarExtraAccessDemoApp.swift
+++ Demo/Demo/MenuBarExtraAccessDemoApp.swift
@@ -99,15 +99,24 @@ struct ContentView: View {
     @Binding var isMenu4Presented: Bool
     
     var body: some View {
-        HStack(spacing: 20) {
-            MenuStateView(num: 4, isMenuPresented: $isMenu4Presented)
-            MenuStateView(num: 3, isMenuPresented: $isMenu3Presented)
-            MenuStateView(num: 2, isMenuPresented: $isMenu2Presented)
-            MenuStateView(num: 1, isMenuPresented: $isMenu1Presented)
-            MenuStateView(num: 0, isMenuPresented: $isMenu0Presented)
+        VStack(spacing: 40) {
+            HStack(spacing: 20) {
+                MenuStateView(num: 4, isMenuPresented: $isMenu4Presented)
+                MenuStateView(num: 3, isMenuPresented: $isMenu3Presented)
+                MenuStateView(num: 2, isMenuPresented: $isMenu2Presented)
+                MenuStateView(num: 1, isMenuPresented: $isMenu1Presented)
+                MenuStateView(num: 0, isMenuPresented: $isMenu0Presented)
+            }
+            
+            Text("""
+                Try opening and closing the menus in the status bar. The toggles will update in response because the state binding is being updated. Clicking on the toggles will also open or close the menus by setting the binding value.")
+                
+                Note that due to how Apple implemented MenuBarExtra, menu-based status items hijack the main runloop and therefore you won't see state update here if you click on their status bar button. Additionally, they cannot be dismissed by setting the binding to false because no SwiftUI date updates occur while the menu is open - the user must select a menu item or dismiss the menu. But the menu can be opened programmatically by setting the binding to true.
+                """
+            )
         }
         .padding()
-        .frame(minWidth: 380, minHeight: 160)
+        .frame(minWidth: 400, minHeight: 350)
     }
     
     struct MenuStateView: View {
