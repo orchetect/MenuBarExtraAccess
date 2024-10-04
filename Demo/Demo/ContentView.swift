36f369291021315b310dd5e940e031439d10572e
Steffan Andrews <stef@orchetect.com>
2024-10-03 21:37:55 -0700

    Demo project cleanup

---
+++ Demo/Demo/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  MenuBarExtraAccessDemoApp.swift
+//  MenuBarExtraAccess • https://github.com/orchetect/MenuBarExtraAccess
+//  © 2023 Steffan Andrews • Licensed under MIT License
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Binding var isMenu0Presented: Bool
+    @Binding var isMenu1Presented: Bool
+    @Binding var isMenu2Presented: Bool
+    @Binding var isMenu3Presented: Bool
+    @Binding var isMenu4Presented: Bool
+    
+    var body: some View {
+        VStack(spacing: 40) {
+            HStack(spacing: 20) {
+                MenuStateView(num: 4, isMenuPresented: $isMenu4Presented)
+                MenuStateView(num: 3, isMenuPresented: $isMenu3Presented)
+                MenuStateView(num: 2, isMenuPresented: $isMenu2Presented)
+                MenuStateView(num: 1, isMenuPresented: $isMenu1Presented)
+                MenuStateView(num: 0, isMenuPresented: $isMenu0Presented)
+            }
+            
+            Text(
+                """
+                Try opening and closing the menus in the status bar. The toggles will update in response because the state binding is being updated. Clicking on the toggles will also open or close the menus by setting the binding value.")
+                
+                Note that due to how Apple implemented MenuBarExtra, menu-based status items hijack the main runloop and therefore you won't see state update here if you click on their status bar button. Additionally, they cannot be dismissed by setting the binding to false because no SwiftUI state updates occur while the menu is open - the user must select a menu item or dismiss the menu. But the menu can be opened programmatically by setting the binding to true.
+                """
+            )
+        }
+        .padding()
+        .frame(minWidth: 400, minHeight: 350)
+    }
+    
+    struct MenuStateView: View {
+        let num: Int
+        @Binding var isMenuPresented: Bool
+        
+        var body: some View {
+            VStack(spacing: 20) {
+                Image(systemName: "\(num).circle.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                Toggle("", isOn: $isMenuPresented)
+                    .toggleStyle(.switch)
+                    .labelsHidden()
+            }
+        }
+    }
+}
--- Demo/Demo/MenuBarExtraAccessDemoApp.swift
+++ Demo/Demo/MenuBarExtraAccessDemoApp.swift
@@ -18,6 +18,8 @@ struct MenuBarExtraAccessDemoApp: App {
     @State var menu0StatusItem: NSStatusItem?
     
     var body: some Scene {
+        // MARK: - Info Window
+        
         WindowGroup {
             ContentView(
                 isMenu0Presented: $isMenu0Presented,
@@ -26,7 +28,10 @@ struct MenuBarExtraAccessDemoApp: App {
                 isMenu3Presented: $isMenu3Presented,
                 isMenu4Presented: $isMenu4Presented
             )
-        }.windowResizability(.contentSize)
+        }
+        .windowResizability(.contentSize)
+        
+        // MARK: - MenuBarExtra Scenes
         
         // 💡 NOTE: There are 5 menu extras here simply to demonstrate (and test)
         // various implementations of MenuBarExtra
@@ -35,19 +40,21 @@ struct MenuBarExtraAccessDemoApp: App {
         // and dragging them), the indexes still remain consistent with the order in which
         // the MenuBarExtra definitions appear below.
         
-        // standard menu
+        // MARK: Standard Menu
+        
         MenuBarExtra("Menu: Index 0", systemImage: "0.circle.fill") {
             Button("Menu Item A") { print("Menu Item A") }
             Button("Menu Item B") { print("Menu Item B") }
         }
         .menuBarExtraAccess(index: 0, isPresented: $isMenu0Presented) { statusItem in
-            // can do one-time setup of NSStatusItem here
-            // or if access to the NSStatusItem is needed later, it may be stored in a local state var
+            // can do one-time setup of NSStatusItem here or if access to it
+            // is needed later, it may be stored in a local state var like this:
             menu0StatusItem = statusItem
         }
         .menuBarExtraStyle(.menu)
         
-        // standard menu using named image
+        // MARK: Standard menu using named image
+        
         MenuBarExtra("Menu: Index 1", image: "1.circle.fill") {
             Button("Menu Item A") { print("Menu Item A") }
             Button("Menu Item B") { print("Menu Item B") }
@@ -59,26 +66,27 @@ struct MenuBarExtraAccessDemoApp: App {
         .menuBarExtraAccess(index: 1, isPresented: $isMenu1Presented)
         .menuBarExtraStyle(.menu)
         
-        // window-style using systemImage
+        // MARK: Window-style using systemImage
+        
         MenuBarExtra("Menu: Index 2", systemImage: "2.circle.fill") {
             MenuBarView(index: 2, isMenuPresented: $isMenu2Presented)
         }
         .menuBarExtraAccess(index: 2, isPresented: $isMenu2Presented)
         .menuBarExtraStyle(.window)
         
-        // window-style using named image
+        // MARK: Window-style using named image
+        
         MenuBarExtra("Menu: Index 3", image: "3.circle.fill") {
             MenuBarView(index: 3, isMenuPresented: $isMenu3Presented)
                 .introspectMenuBarExtraWindow(index: 3) { window in
-                    // just to demonstrate introspection, but animationBehavior
-                    // shouldn't typically be modified for a MenuBarExtra window
-                    window.animationBehavior = .alertPanel
+                    window.alphaValue = 0.5
                 }
         }
         .menuBarExtraAccess(index: 3, isPresented: $isMenu3Presented)
         .menuBarExtraStyle(.window)
         
-        // window-style using custom label
+        // MARK: Window-style using custom label
+        
         MenuBarExtra {
             MenuBarView(index: 4, isMenuPresented: $isMenu4Presented)
         } label: {
@@ -87,69 +95,5 @@ struct MenuBarExtraAccessDemoApp: App {
         }
         .menuBarExtraAccess(index: 4, isPresented: $isMenu4Presented)
         .menuBarExtraStyle(.window)
-    }
-}
-
-struct MenuBarView: View {
-    let index: Int
-    @Binding var isMenuPresented: Bool
-    
-    var body: some View {
-        VStack(spacing: 40) {
-            Image(systemName: "\(index).circle.fill")
-                .resizable()
-                .foregroundColor(.secondary)
-                .frame(width: 80, height: 80)
-            Button("Close Menu") {
-                isMenuPresented = false
-            }
-        }
-        .padding()
-        .frame(width: 250, height: 300)
-    }
-}
-
-struct ContentView: View {
-    @Binding var isMenu0Presented: Bool
-    @Binding var isMenu1Presented: Bool
-    @Binding var isMenu2Presented: Bool
-    @Binding var isMenu3Presented: Bool
-    @Binding var isMenu4Presented: Bool
-    
-    var body: some View {
-        VStack(spacing: 40) {
-            HStack(spacing: 20) {
-                MenuStateView(num: 4, isMenuPresented: $isMenu4Presented)
-                MenuStateView(num: 3, isMenuPresented: $isMenu3Presented)
-                MenuStateView(num: 2, isMenuPresented: $isMenu2Presented)
-                MenuStateView(num: 1, isMenuPresented: $isMenu1Presented)
-                MenuStateView(num: 0, isMenuPresented: $isMenu0Presented)
-            }
-            
-            Text("""
-                Try opening and closing the menus in the status bar. The toggles will update in response because the state binding is being updated. Clicking on the toggles will also open or close the menus by setting the binding value.")
-                
-                Note that due to how Apple implemented MenuBarExtra, menu-based status items hijack the main runloop and therefore you won't see state update here if you click on their status bar button. Additionally, they cannot be dismissed by setting the binding to false because no SwiftUI state updates occur while the menu is open - the user must select a menu item or dismiss the menu. But the menu can be opened programmatically by setting the binding to true.
-                """
-            )
-        }
-        .padding()
-        .frame(minWidth: 400, minHeight: 350)
-    }
-    
-    struct MenuStateView: View {
-        let num: Int
-        @Binding var isMenuPresented: Bool
-        
-        var body: some View {
-            VStack(spacing: 20) {
-                Image(systemName: "\(num).circle.fill")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-                Toggle("", isOn: $isMenuPresented)
-                    .toggleStyle(.switch)
-                    .labelsHidden()
-            }
-        }
     }
 }
