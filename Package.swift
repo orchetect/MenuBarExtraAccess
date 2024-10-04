a9053c366c907f81ed30eaf82bc1b978d7f38925
Steffan Andrews <stef@orchetect.com>
2024-10-03 22:01:05 -0700

    Cleanup

--- Package.swift
+++ Package.swift
@@ -6,17 +6,10 @@ let package = Package(
     name: "MenuBarExtraAccess",
     platforms: [.macOS(.v10_15)],
     products: [
-        .library(
-            name: "MenuBarExtraAccess",
-            targets: ["MenuBarExtraAccess"]),
+        .library(name: "MenuBarExtraAccess", targets: ["MenuBarExtraAccess"])
     ],
-    dependencies: [],
     targets: [
-        .target(
-            name: "MenuBarExtraAccess",
-            dependencies: []),
-        .testTarget(
-            name: "MenuBarExtraAccessTests",
-            dependencies: ["MenuBarExtraAccess"]),
+        .target(name: "MenuBarExtraAccess"),
+        .testTarget(name: "MenuBarExtraAccessTests", dependencies: ["MenuBarExtraAccess"])
     ]
 )
